3e3acdcfe25528c1dfd57156ec96725fd59dd65e
yy4382 <yy4382@outlook.com>
2023-06-02 20:54:26 +0800

    添加了对吃子的支持，优化了显示方式

--- main.py
+++ main.py
@@ -23,7 +23,70 @@ class GoCore:
         else:
             self.board[i][j] = self.current_player
             self.current_player = 3 - self.current_player
+            self.update_status()
             return True
+
+    def get_side_stone(self, i, j, target_player, stones):
+        if i > 0 and self.board[i - 1][j] == target_player and (not (i - 1, j) in stones):
+            stones.append((i - 1, j))
+            self.get_side_stone(i - 1, j, target_player, stones)
+        if i < self.basic_attributes.board_size - 1 and self.board[i + 1][j] == target_player and (
+                not (i + 1, j) in stones):
+            stones.append((i + 1, j))
+            self.get_side_stone(i + 1, j, target_player, stones)
+        if j > 0 and self.board[i][j - 1] == target_player and (not (i, j - 1) in stones):
+            stones.append((i, j - 1))
+            self.get_side_stone(i, j - 1, target_player, stones)
+        if j < self.basic_attributes.board_size - 1 and self.board[i][j + 1] == target_player and (
+                not (i, j + 1) in stones):
+            stones.append((i, j + 1))
+            self.get_side_stone(i, j + 1, target_player, stones)
+
+    def check_kill(self, i, j):
+        side_stones = [(i, j)]
+        self.get_side_stone(i, j, self.current_player, side_stones)
+        for stone in side_stones:
+            left = stone[0] > 0 and self.board[stone[0] - 1][stone[1]] == 0
+            right = stone[0] < self.basic_attributes.board_size - 1 and self.board[stone[0] + 1][stone[1]] == 0
+            up = stone[1] > 0 and self.board[stone[0]][stone[1] - 1] == 0
+            down = stone[1] < self.basic_attributes.board_size - 1 and self.board[stone[0]][stone[1] + 1] == 0
+            if left or right or up or down:
+                return False, side_stones
+        return True, side_stones
+        """这一段可以获取这一片棋具体有多少气，不过这里目前只要判断有没有就行，这个说不定之后写ai的时候有用
+        qi_stones = []
+        qi = 0
+        for stone in side_stones:
+            if stone[0] > 0 and self.board[stone[0] - 1][stone[1]] == 0 and (
+                    not self.board[stone[0] - 1][stone[1]] in qi_stones):
+                qi_stones.append((stone[0] - 1, stone[1]))
+                qi += 1
+            if stone[0] < self.basic_attributes.board_size - 1 and self.board[stone[0] + 1][stone[1]] == 0 and (
+                    not self.board[stone[0] + 1][stone[1]] in qi_stones):
+                qi_stones.append((stone[0] + 1, stone[1]))
+                qi += 1
+            if stone[1] > 0 and self.board[stone[0]][stone[1] - 1] == 0 and (
+                    not self.board[stone[0]][stone[1] - 1] in qi_stones):
+                qi_stones.append((stone[0], stone[j - 1]))
+                qi += 1
+            if stone[1] < self.basic_attributes.board_size - 1 and self.board[stone[0]][stone[1] + 1] == 0 and (
+                    not self.board[stone[0]][stone[1] + 1] in qi_stones):
+                qi_stones.append((stone[0], stone[j + 1]))
+                qi += 1
+        """
+
+    def update_status(self):
+        checked_stone = []
+        for i in range(self.basic_attributes.board_size):
+            for j in range(self.basic_attributes.board_size):
+                if self.board[i][j] == self.current_player and (not (i, j) in checked_stone):
+                    (bo, st) = self.check_kill(i, j)
+                    for item in st:
+                        checked_stone.append(item)
+                    if bo:
+                        print(f"kill {st}")
+                        for item in st:
+                            self.parent.board.remove_stone(item[0], item[1])
 
 
 class GoBoard(tk.Frame):
@@ -68,20 +131,20 @@ class GoBoard(tk.Frame):
                                    self.basic_attributes.board_coordinates[i][self.board_size - 1][1])
             # draw stars
             if i == 3 or i == 9 or i == 15:
-                self.board.create_oval(self.basic_attributes.board_coordinates[i][3][0] - self.cell_size / 8,
-                                       self.basic_attributes.board_coordinates[i][3][1] - self.cell_size / 8,
-                                       self.basic_attributes.board_coordinates[i][3][0] + self.cell_size / 8,
-                                       self.basic_attributes.board_coordinates[i][3][1] + self.cell_size / 8,
+                self.board.create_oval(self.basic_attributes.board_coordinates[i][3][0] - self.cell_size / 10,
+                                       self.basic_attributes.board_coordinates[i][3][1] - self.cell_size / 10,
+                                       self.basic_attributes.board_coordinates[i][3][0] + self.cell_size / 10,
+                                       self.basic_attributes.board_coordinates[i][3][1] + self.cell_size / 10,
                                        fill="black")
-                self.board.create_oval(self.basic_attributes.board_coordinates[i][9][0] - self.cell_size / 8,
-                                       self.basic_attributes.board_coordinates[i][9][1] - self.cell_size / 8,
-                                       self.basic_attributes.board_coordinates[i][9][0] + self.cell_size / 8,
-                                       self.basic_attributes.board_coordinates[i][9][1] + self.cell_size / 8,
+                self.board.create_oval(self.basic_attributes.board_coordinates[i][9][0] - self.cell_size / 10,
+                                       self.basic_attributes.board_coordinates[i][9][1] - self.cell_size / 10,
+                                       self.basic_attributes.board_coordinates[i][9][0] + self.cell_size / 10,
+                                       self.basic_attributes.board_coordinates[i][9][1] + self.cell_size / 10,
                                        fill="black")
-                self.board.create_oval(self.basic_attributes.board_coordinates[i][15][0] - self.cell_size / 8,
-                                       self.basic_attributes.board_coordinates[i][15][1] - self.cell_size / 8,
-                                       self.basic_attributes.board_coordinates[i][15][0] + self.cell_size / 8,
-                                       self.basic_attributes.board_coordinates[i][15][1] + self.cell_size / 8,
+                self.board.create_oval(self.basic_attributes.board_coordinates[i][15][0] - self.cell_size / 10,
+                                       self.basic_attributes.board_coordinates[i][15][1] - self.cell_size / 10,
+                                       self.basic_attributes.board_coordinates[i][15][0] + self.cell_size / 10,
+                                       self.basic_attributes.board_coordinates[i][15][1] + self.cell_size / 10,
                                        fill="black")
             # write the coordinates
             self.board.create_text(self.basic_attributes.board_coordinates[i][0][0],
@@ -103,8 +166,8 @@ class GoBoard(tk.Frame):
         if i < 0 or i >= self.board_size or j < 0 or j >= self.board_size:
             print("invalid click")
             return
-        print(f"player:{self.core.current_player} ({i}, {j})")
-        
+        print(f"player{self.core.current_player}: ({i}, {j})")
+
         if self.core.place_stone(i, j):
             self.draw_stone(i, j, self.core.current_player)
 
@@ -115,13 +178,16 @@ class GoBoard(tk.Frame):
                                    self.basic_attributes.board_coordinates[i][j][1] - self.cell_size / 3,
                                    self.basic_attributes.board_coordinates[i][j][0] + self.cell_size / 3,
                                    self.basic_attributes.board_coordinates[i][j][1] + self.cell_size / 3,
-                                   fill="black")
+                                   fill="black", tags=f"{i},{j}")
         elif current_player == 1:
             self.board.create_oval(self.basic_attributes.board_coordinates[i][j][0] - self.cell_size / 3,
                                    self.basic_attributes.board_coordinates[i][j][1] - self.cell_size / 3,
                                    self.basic_attributes.board_coordinates[i][j][0] + self.cell_size / 3,
                                    self.basic_attributes.board_coordinates[i][j][1] + self.cell_size / 3,
-                                   fill="white")
+                                   fill="white", tags=f"{i},{j}")
+
+    def remove_stone(self, i, j):
+        self.board.delete(f"{i},{j}")
 
 
 class Go(tk.Tk):
